e042d224c9c174f0a926c76203bea5bb9bfb3c7a
Denys Halytskyi <denys.halytskyi17@gmail.com>
2025-03-30 21:14:22 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
+import os
+import traceback
+from typing import Optional, Type
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> "CleanUpFile":
+        return self
+
+    def __exit__(self, exc_type: Optional[Type[BaseException]],
+                 exc_val: Optional[BaseException],
+                 exc_tb: Optional[traceback]) -> None:
+        if exc_type is not None:
+            print(f"Виникло виключення: {exc_type}, {exc_val}")
+
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+            print(f"Файл {self.filename} був видалений.")
